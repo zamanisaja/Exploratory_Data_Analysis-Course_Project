2a3c10e27044d9550c5694d888cf268e27d5d67f
Sadjad Zamani <zamani.saja@gmail.com>
2022-04-28 15:55:37 +0200

    plot1
    drawing plot1 complete.

--- run-analysis.R
+++ run-analysis.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 
 if (!file.exists("data")) {
-    dataurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+    dataurl <- "https://d396qusza40orc.cloudfrontnet/exdata%2Fdata%2FNEI_data.zip"
     download.file(dataurl, destfile = "data.zip")
-    unzip("data.zip", exdir="data")
-    file.remove("data.zip")
+    unzip("data.zip", exdir = "data")
+    kfile.remove("data.zip")
 }
 
 NEI <- readRDS("data/summarySCC_PM25.rds")
@@ -13,13 +13,26 @@ SCC <- readRDS("data/Source_Classification_Code.rds")
 NEI$type <- as.factor(NEI$type)
 NEI$year <- as.factor(NEI$year)
 
-df <- NEI %>%
+df1 <- NEI %>%
     group_by(year, type) %>%
     summarise(TotalEmissions = sum(Emissions))
 
+df2 <- NEI %>%
+    group_by(year) %>%
+    summarise(TotalEmissions = sum(Emissions))
+df2$type <- as.factor(rep("SUM", times = 4))
 
-g <- ggplot(data = df)
-
-g +
+# require(gridExtra)
+# can use gridExtra to plot the data
+myplot <- ggplot(data = rbind(df1, df2)) +
     geom_line(aes(x = year, y = TotalEmissions, color = type, group = type)) +
     geom_point(aes(x = year, y = TotalEmissions, color = type))
+
+png("plot1.png")
+plot(
+    x = as.numeric(levels(df2$year)[df2$year]),
+    y = df2$TotalEmissions,
+    pch = 16, col = "black", lwd = 200, cex = 4,
+    xlab = "Year", ylab = "Total Emissions (tons)",
+)
+dev.off()
